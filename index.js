8736051cdd0e5146786deb2fac1ade52da443362
hsalcedo <19m4verik19@gmail.com>
2023-05-18 13:02:18 -0600

    Project creation

---
+++ index.js
@@ -0,0 +1,19 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+ReactDOM.render(
+    <div>
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <p>This is a red paragraph.</p>
+    <strong>This text is important!.</strong>
+    <i>This text is italic.</i>
+    <a href="https://www.w3schools.com">This is a link</a>
+    <img src="https://e7.pngegg.com/pngimages/410/73/png-clipart-jumpman-air-jordan-graphics-logo-nike-white-text.png" width="150" height="150"></img>
+    </div>,
+    document.getElementById("root")
+)
